de499951b680d2539705f1ff691fbd95aefa5e4e
Morgan Zolko <morganzolkart@gmail.com>
2019-03-20 13:27:11 -0400

    LGBY-63:Added Rating system info popup

--- client/src/components/dashboard/shared/steps/JobComplete.jsx
+++ client/src/components/dashboard/shared/steps/JobComplete.jsx
@@ -1,43 +1,98 @@
 import React from 'react';
-import Rating from 'react-rating'
+import Rating from 'react-rating';
 import { constant } from '../../../../shared/index';
 
 export default class JobComplete extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-    }
+      isInfoHovered: true
+    };
+
+    this.hoverOnInfo = this.hoverOnInfo.bind(this);
+    this.hoverOffInfo = this.hoverOffInfo.bind(this);
   }
 
-  handleClick() {
+  handleClick() {}
+
+  hoverOnInfo() {
+    console.log('hovered');
+    this.setState({
+      isInfoHovered: true
+    });
+  }
+
+  hoverOffInfo() {
+    this.setState({
+      isInfoHovered: false
+    });
+  }
+
+  showStarGroup(fullStar) {
+    const stars = [];
+
+    for (let i = 1; i <= 4; i++) {
+      let element = null;
+
+      if (i <= fullStar) {
+        element = <i key={i} className="fa fa-star fa-2x" aria-hidden="true" />;
+      } else {
+        element = <i key={i} className="fa fa-star-o fa-2x" aria-hidden="true" />;
+      }
+
+      stars.push(element);
+    }
+
+    return stars;
   }
 
   render() {
-    const isSeeker = this.props.role === constant['ROLE']['SEEKER']
+    const isSeeker = this.props.role === constant['ROLE']['SEEKER'];
+    const { isInfoHovered } = this.state;
 
     return (
       <div>
         <div className="status-content mt-45">
-          {
-            isSeeker ?
-              'Nice work! You\'ve completed this job.'
-            :
-              'All milestones for this job have been completed.'
-          }
+          {isSeeker ? "Nice work! You've completed this job." : 'All milestones for this job have been completed.'}
         </div>
         <div className="status-content mt-15">
           Please rate your experience with this
           {isSeeker ? ' candidate' : ' hiring manager'}
+          <span className="rating-info-icon">
+            <i onMouseEnter={this.hoverOnInfo} onMouseLeave={this.hoverOffInfo} className="fa fa-question-circle-o ml-5" aria-hidden="true" />
+            {isInfoHovered && (
+              <div className="rating-info-box pl-10">
+                <div className="each-detail">
+                  {this.showStarGroup(1)}
+                  <span>
+                    <h5> Would never work with them again</h5>
+                  </span>
+                </div>
+                <div className="each-detail">
+                  {this.showStarGroup(2)}
+                  <span>
+                    <h5> Would consider working with them again</h5>
+                  </span>
+                </div>
+                <div className="each-detail">
+                  {this.showStarGroup(3)}
+                  <span>
+                    <h5> Would want to work with them again</h5>
+                  </span>
+                </div>
+                <div className="each-detail">
+                  {this.showStarGroup(4)}
+                  <span>
+                    <h5> Would definitely want to work with them again</h5>
+                  </span>
+                </div>
+              </div>
+            )}
+          </span>
         </div>
-        <div className='rating-box'>
-          <Rating
-            className="star-rating ml-10"
-            emptySymbol="fa fa-star-o fa-2x"
-            fullSymbol="fa fa-star fa-2x"
-            stop={4}
-            initialRating={3}
-          />
-          <button type="button" className="btn ml-20 btn-primary" onClick={this.handleClick}>
+        <div className="rating-box mt-20 ml-10">
+          <Rating className="star-rating ml-10" emptySymbol="fa fa-star-o fa-2x" fullSymbol="fa fa-star fa-2x" stop={4} initialRating={3} />
+          <button type="button" className="btn ml-30 btn-primary" onClick={this.handleClick}>
             Save rating
           </button>
         </div>

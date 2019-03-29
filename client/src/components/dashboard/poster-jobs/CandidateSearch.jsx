26b71639bf9ba9f60a6831d07f1c574b7f9194b3
Morgan Zolko <morganzolkart@gmail.com>
2019-03-29 09:50:21 -0400

    LGBY-19: Add candidates search filter box

--- client/src/components/dashboard/poster-jobs/CandidateSearch.jsx
+++ client/src/components/dashboard/poster-jobs/CandidateSearch.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { Link } from 'react-router';
 import { ToastContainer } from 'react-toastify';
 import Pagination from 'react-js-pagination';
+import { Truncate } from 'react-read-more';
+import Select from 'react-select'
 
 import { Dashboard } from '../../index';
 import { constant, utils } from '../../../shared/index';
@@ -13,10 +15,57 @@ export default class CandidateSearch extends React.Component {
       totalCandidateCount: 0,
       activePage: 1,
       candidateData: [],
-      isResponse: false
+      isResponse: false,
+      practice_area_dropdown: [],
+      state_dropdown: [],
+      practiceAreas: [],
+      states: []
     };
 
     this.handlePageChange = this.handlePageChange.bind(this);
+    this.setMultiSelectValues = this.setMultiSelectValues.bind(this);
+  }
+
+  componentDidMount() {
+    let that = this;
+    let userRole = constant['ROLE']['POSTER'];
+    const practiceAreaList = utils.getListData('practice_areas')
+    const statesList = utils.getListData('states')
+    const practiceAreas = [];
+    const states = [];
+
+    for (let pAreasObj of practiceAreaList) {
+      practiceAreas.push({value: pAreasObj['_id'], label: pAreasObj['name']});
+    }
+
+    for (let statesObj of statesList) {
+      states.push({value: statesObj['_id'], label: statesObj['name']});
+    }
+
+    that.setState({
+      practice_area_dropdown: practiceAreas,
+      state_dropdown: states
+    })
+
+    utils.apiCall('GET_CANDIDATES_DATA', {}, function(err, response) {
+      if (err) {
+        utils.flashMsg('show', 'Error while getting Candidates data');
+        utils.logger('error', 'Get Candidate Data Error -->', err)
+      } else {
+        if (utils.isResSuccess(response)) {
+
+          that.setState({
+            candidateData: response.data.data
+          })
+        }
+      }
+    })
+  }
+
+  setMultiSelectValues (val, key) {
+    var stateObj = this.state;
+    stateObj[key] = val;
+    this.setState(stateObj)
   }
 
   handlePageChange(pageNumber) {
@@ -52,21 +101,12 @@ export default class CandidateSearch extends React.Component {
     )
   }
 
-  componentDidMount() {
-    let that = this;
-    let userRole = constant['ROLE']['POSTER'];
-
-    utils.apiCall('GET_CANDIDATES_DATA'), null, function(err, response) {
-      if (err) {
-        utils.flashMsg('show', 'Error while getting Candidates data');
-        utils.logger('error', 'Get Candidate Data Error -->', err)
-      } else {
-        if (utils.isResSuccess(response)) {
-
-          this.setState({
-            candidateData: response.data
-          })
-        }
+  userDetailLink(userId) {
+    let routesPath = constant['ROUTES_PATH'];
+    return {
+      pathname: routesPath['PROFILE'] + '/' + userId,
+      state: {
+        jobId: this.props.jobId
       }
     }
   }
@@ -92,7 +132,7 @@ export default class CandidateSearch extends React.Component {
   }
 
   render() {
-    const {candidateData} = this.state
+    const {candidateData, practiceAreas, states} = this.state
 
     return (
       <Dashboard>
@@ -105,40 +145,66 @@ export default class CandidateSearch extends React.Component {
               <p>Candidate Search</p>
             </ol>
           </div>
-          <div className="job-search-card mb-30">
-            <div className="status-content mt-45">
-              <div className="candidates-applied">
-                <h6 className="mb-30">Here are avaliable candidates.</h6>
-                {
-                  candidateData.map((item, index) => (
-                    <div key={index} className="candidate-data">
-                      <div className="pull-left pr-30">
-                        <img src={this.getPhotoUrl(item.job_seeker_info.network.photo)} alt="profile-img" onError={this.profileImgError} />
-                      </div>
-                      <div className="right-panel p-0">
-                        <div className="row m-0">
-                          <Link to={this.userDetailLink(item._id)} className="job-title mb-10">
-                            {item.first_name + ' ' + item.last_name}
-                          </Link>
+          <div className="bg-white widget-wrapper clearfix">
+            <div className="search-filter-box m-30">
+              <div className="col-sm-5">
+                <div className="form-group">
+                  <label className="control-label">Practice Area</label>
+                  <Select multi closeOnSelect = {false} onBlurResetsInput = {true} autosize = {false}
+                    onChange={(val) => this.setMultiSelectValues(val, 'practiceAreas')}
+                    options={this.state.practice_area_dropdown}
+                    placeholder="Select Practice Area(s)"
+                    value={practiceAreas} />
+                </div>
+              </div>
+              <div className="col-sm-5">
+                <div className="form-group">
+                  <label className="control-label">State</label>
+                  <Select multi closeOnSelect = {false} onBlurResetsInput = {true} autosize = {false}
+                    onChange={(val) => this.setMultiSelectValues(val, 'states')}
+                    options={this.state.state_dropdown}
+                    placeholder="Select State(s)"
+                    value={states} />
+                </div>
+              </div>
+              <button type="button" className="btn ml-10 btn-primary mt-30" onClick={this.handleSearch}>
+                Search
+              </button>
+            </div>
+            <div className="candidate-search-card mb-30">
+              <div className="status-content mt-45">
+                <div className="candidates-applied column-flex">
+                  {
+                    candidateData.map((item, index) => (
+                      <div key={index} className="candidate-data">
+                        <div className="pull-left pr-30">
+                          <img src={this.getPhotoUrl(item.job_seeker_info.network.photo)} alt="profile-img" onError={this.profileImgError} />
                         </div>
-                        <div className="row sub-titles">
-                          {
-                            this.getPracticeAreas(item.job_seeker_info.basic_profile.practice_area_id)
-                          }
-                          {
-                            this.getLocations(item.job_seeker_info.job_profile.willing_to_work_location_id)
-                          }
-                          <span className="clearfix"></span>
+                        <div className="right-panel p-0">
+                          <div className="row m-0">
+                            <Link to={this.userDetailLink(item._id)} className="job-title mb-10">
+                              {item.first_name + ' ' + item.last_name}
+                            </Link>
+                          </div>
+                          <div className="row sub-titles">
+                            {
+                              this.getPracticeAreas(item.job_seeker_info.basic_profile.practice_area_id)
+                            }
+                            {
+                              this.getLocations(item.job_seeker_info.job_profile.willing_to_work_location_id)
+                            }
+                            <span className="clearfix"></span>
+                          </div>
+                          <p className="para mt-10 mb-20">
+                            <Truncate lines={2} ellipsis={<span>... <Link className="more" to={this.userDetailLink(item._id)}>more</Link></span>}>
+                              {item.job_seeker_info.network.about_lawyer}
+                            </Truncate>
+                          </p>
                         </div>
-                        <p className="para mt-10 mb-20">
-                          <Truncate lines={2} ellipsis={<span>... <Link className="more" to={this.userDetailLink(item._id)}>more</Link></span>}>
-                            {item.job_seeker_info.network.about_lawyer}
-                          </Truncate>
-                        </p>
                       </div>
-                    </div>
-                  ))
-                }
+                    ))
+                  }
+                </div>
               </div>
             </div>
           </div>

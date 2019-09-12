8f89ceb07783fcf8c2ba6ae1428b93fd73891f5c
webdev778 <railsruby80@gmail.com>
2019-09-12 15:49:12 +0200

    CORS issue

--- server/server.js
+++ server/server.js
@@ -24,7 +24,7 @@ function start() {
   //To prevent errors from Cross Origin Resource Sharing, we will set our headers to allow CORS with middleware like so:
   app.use(function (req, res, next) {
     res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Credentials', 'true');
+    // res.setHeader('Access-Control-Allow-Credentials', 'true');
     res.setHeader('Access-Control-Allow-Methods', 'GET, HEAD, OPTIONS, POST, PUT, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers, Origin, Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers');
 

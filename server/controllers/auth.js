247d200ff8c7d88da1ef53f93088fd44ccd1dff3
webdev778 <railsruby80@gmail.com>
2019-09-12 14:42:08 +0200

    changed auth/user with meaningful content

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -3,10 +3,14 @@ var rfr = require('rfr');
 var utils = rfr('/server/shared/utils');
 
 const login = (req, res) => {
+
+  // const userId = 11;
+  // const counselorId = 203;
+
   const result = {
-    id: 9999,
-    firstName: 'Aaron',
-    lastName: 'Balick',
+    id: 203,
+    firstName: 'Anthony',
+    lastName: 'Maki',
     isCounsellor: true,
   };
 

44649cb8647ff2f050c6a247a81fe7031af56ddb
webdev778 <railsruby80@gmail.com>
2019-09-24 16:15:54 -0600

    added staging config  in sequelize

--- server/config/database.js
+++ server/config/database.js
@@ -23,6 +23,14 @@ module.exports = {
     dialect: 'postgres',
     operatorsAliases: false
   },
+  staging: {
+    username: parsedEnv.DB_USER,
+    password: parsedEnv.DB_PASS,
+    database: parsedEnv.DB_NAME,
+    host: parsedEnv.DB_HOST,
+    dialect: 'postgres',
+    operatorsAliases: false
+  },
   production: {
     username: parsedEnv.DB_USER,
     password: parsedEnv.DB_PASS,
